f41227de358844097d62a10f415b743a7aaa5565
Anton Ehrmanntraut <anton.ehrmanntraut@uni-wuerzburg.de>
2025-04-28 00:50:04 +0200

    adjust jobs

--- prepare_slurm_batch.py
+++ prepare_slurm_batch.py
@@ -32,14 +32,17 @@ for m in [
     "/data/42-julia-hpc-rz-lsx/juw57zv/models/1B_LLM2Vec_2048/iter-01430512-ckpt_1B_sim_new",
     "/data/42-julia-hpc-rz-lsx/juw57zv/models/7B_LLM2Vec_2048/iter-01430512-ckpt_7B_mntp_sim",
     "/data/42-julia-hpc-rz-lsx/juw57zv/models/7B_LLM2Vec_2048/iter-01430512-ckpt_7B_sim",
-    "/data/42-julia-hpc-rz-lsx/juw57zv/models/1B_LLaMA_LLM2Vec/Llama-3.2-1B_llama1B_mntp_sim",
-    "/data/42-julia-hpc-rz-lsx/juw57zv/models/1B_LLaMA_LLM2Vec/Llama-3.2-1B_llama1B_sim"
     ]:
     jobs.append(["train_args=a100", "+task=niah_germanquad", "+model=llm2vec_for_mntp-10000_simcse-200", f"model.model_name={m}"])
     jobs.append(["train_args=a100", "+task=niah_germanquad", "+model=llm2vec_for_mntp-10000_simcse-200", f"model.model_name={m}", "+model.model_config_args.rope_scaling={type:'dynamic',factor:2}"])
     jobs.append(["train_args=a100", "+task=niah_germanquad", "+model=llm2vec_for_mntp-10000_simcse-200", f"model.model_name={m}", "+model.model_config_args.rope_scaling={type:'dynamic',factor:4}"])
     jobs.append(["train_args=a100", "+task=niah_germanquad", "+model=llm2vec_for_mntp-10000_simcse-200", f"model.model_name={m}", "+model.model_config_args.rope_scaling={type:'dynamic',factor:8}"])
 
+for m in [
+    "/data/42-julia-hpc-rz-lsx/juw57zv/models/1B_LLaMA_LLM2Vec/Llama-3.2-1B_llama1B_mntp_sim",
+    "/data/42-julia-hpc-rz-lsx/juw57zv/models/1B_LLaMA_LLM2Vec/Llama-3.2-1B_llama1B_sim"
+    ]: 
+    jobs.append(["train_args=a100", "+task=niah_germanquad", "+model=llm2vec_for_mntp-10000_simcse-200", f"model.model_name={m}"])
 
 with open('slurm_template.jinja') as f:
     tmpl = Template(f.read())
